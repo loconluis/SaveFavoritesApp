1666e9495776e85aebc522e0b4e84dabf41b5560
luislocon <luis.locon@gmail.com>
2017-09-17 01:38:39 -0600

    añadiendo funcionalidad al controlador de guardar con mongoose

--- api-restful-favoritos/controllers/favorito.controller.js
+++ api-restful-favoritos/controllers/favorito.controller.js
@@ -1,4 +1,7 @@
 'use strict'
+// Importes
+const Favorite = require('../models/favorite.model')
+
 // controladores funciones que manejan las funcionalidades de la ruta
 function prueba (req, res) {
   let nombre = req.params.nombre
@@ -7,10 +10,7 @@ function prueba (req, res) {
 
 // Obtiene el favorito (single)
 function getFavorite (req, res) {
-  let favoriteID = req.params.id
-  res.status(200).send({
-    data: favoriteID
-  })
+
 }
 
 // Lista los favoritos
@@ -20,9 +20,18 @@ function allFavorites (req, res) {
 
 // Guarda un nuevo favorito
 function saveFavorite (req, res) {
+  let favorite = new Favorite()
   let params = req.body
-  res.status(200).send({
-    favorite: params
+
+  favorite.title = params.title
+  favorite.description = params.description
+  favorite.url = params.url
+
+  favorite.save((err, data) => {
+    if (err) return res.status(500).send({ message: 'Error al guardar' + err })
+
+    // save
+    res.status(200).send({ favorite: data })
   })
 }
 
--- api-restful-favoritos/models/favorite.model.js
+++ api-restful-favoritos/models/favorite.model.js
@@ -2,3 +2,11 @@
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+let FavoriteSchema = Schema({
+  title: String,
+  description: String,
+  url: String
+})
+
+module.exports = mongoose.model('Favorite', FavoriteSchema)
