5e4d85bf8ca290740cfd65a7061279b68eaee955
luislocon <luis.locon@gmail.com>
2017-09-21 23:48:51 -0600

    Añadiendo hints

--- client-favoritos/app/models/favorites.model.ts
+++ client-favoritos/app/models/favorites.model.ts
@@ -1,4 +1,5 @@
 // definiendo un modelo como una clase POO
+// La estructura de los datos que se van a guardar en mongo
 export default class Favorite {
   constructor(public title: string, public description: string, public url: string) {
 
